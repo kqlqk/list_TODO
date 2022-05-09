a432a77c9e7fcdedf0374b116c8af72d6a109d5b
kqlqk <kononchuk99999@gmail.com>
2022-05-09 20:26:05 +0300

    improving sessionFactory

--- src/main/java/me/kqlqk/springBootApp/util/HibernateUtil.java
+++ src/main/java/me/kqlqk/springBootApp/util/HibernateUtil.java
@@ -1,19 +1,15 @@
 package me.kqlqk.springBootApp.util;
 
-import org.apache.catalina.core.StandardService;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HibernateUtil {
+    @Autowired
+    private static SessionFactory sessionFactory;
+
     public static SessionFactory getSessionFactory(){
-        Configuration config = new Configuration();
-        config.configure();
-        SessionFactory sessionFactory = config.buildSessionFactory();
         return sessionFactory;
     }
 }

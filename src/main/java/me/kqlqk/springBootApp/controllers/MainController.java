0262e587caaffedbb77a160c0d3e2a81dfcf41d8
kqlqk <kononchuk99999@gmail.com>
2022-05-25 21:28:01 +0300

    was added remember me button(beta version)

--- src/main/java/me/kqlqk/springBootApp/controllers/MainController.java
+++ src/main/java/me/kqlqk/springBootApp/controllers/MainController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -29,6 +32,9 @@ public class MainController {
 
     @GetMapping
     public String showMainPage(){
+        if(userService.getCurrentUser() != null){
+            return "redirect:/home";
+        }
         return "main-page/mainLoginPage";
     }
 
@@ -38,23 +44,43 @@ public class MainController {
             return "redirect:/home";
         }
         model.addAttribute("userValid", new UserValidation());
+        model.addAttribute("rememberMe", null);
         return "main-page/login";
     }
 
     @PostMapping("/login")
-    public String logIn(@ModelAttribute("userValid") UserValidation userValidation){
+    public String logIn(@ModelAttribute("userValid") UserValidation userValidation,
+                        @ModelAttribute(name = "rememberMe") String rememberMe,
+                        HttpServletResponse response,
+                        HttpServletRequest request){
+
         if(userService.getByEmailOrLogin(userValidation.getLoginObject()) != null &&
                 passwordEncoder.matches(userValidation.getPassword(), userService.getByEmailOrLogin(userValidation.getLoginObject()).getPassword())){
                 if(userService.tryAutoLogin(userValidation.getLoginObject(), userValidation.getPassword())) {
+                    if(rememberMe.equals("on")){
+                        for(Cookie cookie : request.getCookies()){
+                            if(cookie.getName().equals("JSESSIONID")){
+                                cookie.setMaxAge(60 * 60 * 24 * 7);
+                                response.addCookie(cookie);
+                            }
+                        }
+                    }
+                    else{
+                        for(Cookie cookie : request.getCookies()){
+                            if(cookie.getName().equals("JSESSIONID")){
+                                cookie.setMaxAge(-1);
+                                response.addCookie(cookie);
+                            }
+                        }
+                    }
                     return "redirect:/home";
                 }
 
-                return "redirect:/error";
-            }
+            return "redirect:/error";
+        }
         else {
             userValidation.setFormCorrect(false);
         }
-
         return "main-page/login";
     }
 
@@ -81,9 +107,11 @@ public class MainController {
         userService.addNew(userToDB);
 
         if(userService.tryAutoLogin(userToDB.getEmail(), decryptedPassword)){
+
             return "redirect:/home";
         }
 
         return "redirect:/mainPage";
     }
+
 }
